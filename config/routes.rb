9cd5de90321a26b51d3d171748ad4b2ff3701817
Inna Babiichuk <goodluck8vdl@gmail.com>
2016-12-15 02:23:40 +0000

    5.3 forms

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
-
+  
+  post 'static_pages/thank_you'
   # The priority is based ugmail.compon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
