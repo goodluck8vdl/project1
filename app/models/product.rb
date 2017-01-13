6c4f0dbf8808f07ba7a275b93e7f6e0615b3f22c
Inna Babiichuk <goodluck8vdl@gmail.com>
2017-01-13 03:29:53 +0000

    5.8

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,5 +10,9 @@ class Product < ActiveRecord::Base
    def lowest_rating_comment
      comments.rating_asc.first
    end
+   
+   def average_rating
+     comments.average(:rating).to_f
+   end
   	
 end
