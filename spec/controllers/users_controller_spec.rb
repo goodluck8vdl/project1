151f089a94381019e1bffd6fcad1623148ad571d
Inna Babiichuk <goodluck8vdl@gmail.com>
2017-01-18 21:41:52 +0000

    6.4

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
+ before do
+   #let(:user) { User.create!(email: 'test@gmail.com', password: 'testtt') }
+   @user = FactoryGirl.create(:user)
+  end
   
-  let(:user) { User.create!(email: 'test@gmail.com', password: 'testtt') }
-
-  describe 'GET #show' do
-     context 'User is logged in' do
+  describe "GET #show" do
+    
+     context "User is logged in" do
       before  do
-        sign_in user
+        sign_in @user
       end
-    it 'loads correct user details' do
-      get :show, id: user.id
+    it "loads correct user details" do
+      get :show, id: @user.id
        expect(response).to be_success
        expect(response).to have_http_status(200)
-       expect(assigns(:user)).to eq user
+       expect(assigns(:user)).to eq @user
     end
      end
 
-     context 'No user is logged in' do
-       it 'redirects to login' do
-         get :show, id: user.id
+     context "No user is logged in" do
+       it "redirects to login" do
+         get :show, id: @user.id
          expect(response).to redirect_to(new_user_session_path)
        end
      end
