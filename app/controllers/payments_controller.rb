b2113f552d81955a1bd6db8336723b63e7c00415
Inna Babiichuk <goodluck8vdl@gmail.com>
2017-01-19 23:44:56 +0000

    6.6

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,31 @@
+class PaymentsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    token = params[:stripeToken]
+      
+    begin
+      charge = Stripe::Charge.create(
+        :amount => @product.price * 100, # amount in cents, again
+        :currency => "usd",
+        :source => token,
+        :description => params[:stripeEmail]
+          )
+    if charge.paid
+            Order.create(
+              product_id: @product.id,
+				      user_id: @user_id,
+				      total: @product.price
+				      )
+				      UserMailer.paid_success(@user, @product).deliver_now
+				      flash[:success] = "Payment processed successfully"
+    end
+    rescue Stripe::CardError => e
+       # The card has been declined
+			body = e.json_body
+			err = body[:error]
+			flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+		end
+		redirect_to products_path(@product)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   
   post 'static_pages/thank_you'
   
+  post 'payments/create'
+  
   # The priority is based ugmail.compon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
