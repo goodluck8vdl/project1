1187e4a74a0991694a3e03b6c0ec52a7d79865d9
Inna Babiichuk <goodluck8vdl@gmail.com>
2017-01-20 20:15:18 +0000

    6.9

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,12 @@
  $(document).on('ready page:load', function(){
+   $('.img-zoom').elevateZoom({
+     zoomType: "lens",
+     lensShape: "round",
+     lensSize: 180,
+     scrollZoom: true,
+     lensFadeIn: 800,
+     lensFadeOut: 800
+    });
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,
@@ -6,4 +14,4 @@
         return $(this).attr('data-score');
       }
     });
-});
+  });
