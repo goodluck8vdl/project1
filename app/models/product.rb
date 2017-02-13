a8f55328aa2724818f416c6d488c3e6c6a89cb2b
Inna Babiichuk <goodluck8vdl@gmail.com>
2017-02-13 02:58:50 +0000

    changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,6 @@ class Product < ActiveRecord::Base
     validates :name, presence: true
     validates :description, presence: true
 	validates :image_url, presence: true
-	validates :colour, presence: true
 	validates :price, presence: true
     
     
